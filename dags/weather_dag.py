640a7462fac6721fc0ef55c62cc7fab5846bbe5d
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-09-29 06:28:25 +0530

    add dag

---
+++ dags/weather_dag.py
@@ -0,0 +1,7 @@
+from airflow import DAG
+from datetime import timedelta, datetime
+from airflow.providers.http.sensors.http import HttpSensor
+import json
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.operators.python import PythonOperator
+import pandas as pd
---
+++ default_args.py
@@ -0,0 +1,12 @@
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 1, 8),
+    'email': ['mpinildissanayaka@gmail.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 2,
+    'retry_delay': timedelta(minutes=2)
+}

f6d1f8657066c7237d38a00bb390996c731d5cb2
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-12-07 16:26:49 +0530

    add first node

---
+++ dags/utils/default_args.py
@@ -0,0 +1,12 @@
+from datetime import datetime, timedelta
+
+default_args={
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime(2024, 12, 7),
+    "email": ["V7jyT@example.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=2)
+}
--- dags/weather_dag.py
+++ dags/weather_dag.py
@@ -1,7 +1,21 @@
 from airflow import DAG
+from utils import default_args
 from datetime import timedelta, datetime
 from airflow.providers.http.sensors.http import HttpSensor
 import json
 from airflow.providers.http.operators.http import SimpleHttpOperator
 from airflow.operators.python import PythonOperator
 import pandas as pd
+
+
+with DAG(
+    'Weather_DAG',
+    default_args=default_args,
+    description='A simple DAG used to ETL',
+    schedule_interval=timedelta(days=1),
+    tags=['example'],
+) as dag:
+    
+    is_api_ready=HttpSensor(
+        
+    )

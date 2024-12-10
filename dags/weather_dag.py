f8f79abb845ac5b13454d7ef70f30424a11e3bd5
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-12-10 20:59:19 +0530

    add transfor and load node

--- dags/weather_dag.py
+++ dags/weather_dag.py
@@ -7,17 +7,12 @@ from airflow.operators.python import PythonOperator
 import pandas as pd
 import os
 from airflow.providers.http.operators.http import SimpleHttpOperator
+from dotenv import load_dotenv, find_dotenv
 
+load_dotenv(find_dotenv())
 
-def transform_data(task_instance):
-    data = task_instance.xcom_pull(task_ids='extract_weather_data')
-    
-    
-def logging_data(task_instance):
-    data = task_instance.xcom_pull(task_ids='extract_weather_data')    
-    
-    with open('data.txt', 'w') as f:
-        f.write(str(data))
+aws_credentials = os.getenv("AWS_CREDISTIALS")
+
 
 
 def kelvin_to_fahrenheit(temp_in_kelvin):
@@ -25,9 +20,46 @@ def kelvin_to_fahrenheit(temp_in_kelvin):
     return temp_in_fahrenheit
 
 
+def transform_data(task_instance):
+    data = task_instance.xcom_pull(task_ids='extract_weather_data')
 
+    city = data["name"]
+    weather_description = data["weather"][0]['description']
+    temp_farenheit = kelvin_to_fahrenheit(data["main"]["temp"])
+    feels_like_farenheit= kelvin_to_fahrenheit(data["main"]["feels_like"])
+    min_temp_farenheit = kelvin_to_fahrenheit(data["main"]["temp_min"])
+    max_temp_farenheit = kelvin_to_fahrenheit(data["main"]["temp_max"])
+    pressure = data["main"]["pressure"]
+    humidity = data["main"]["humidity"]
+    wind_speed = data["wind"]["speed"]
+    time_of_record = datetime.fromtimestamp(data['dt'] + data['timezone'])
+    sunrise_time = datetime.fromtimestamp(data['sys']['sunrise'] + data['timezone'])
+    sunset_time = datetime.fromtimestamp(data['sys']['sunset'] + data['timezone'])
 
+    transformed_data = {"City": city,
+                        "Description": weather_description,
+                        "Temperature (F)": temp_farenheit,
+                        "Feels Like (F)": feels_like_farenheit,
+                        "Minimun Temp (F)":min_temp_farenheit,
+                        "Maximum Temp (F)": max_temp_farenheit,
+                        "Pressure": pressure,
+                        "Humidty": humidity,
+                        "Wind Speed": wind_speed,
+                        "Time of Record": time_of_record,
+                        "Sunrise (Local Time)":sunrise_time,
+                        "Sunset (Local Time)": sunset_time                        
+                        }
+    
+    transformed_data_list = [transformed_data]
+    df_data = pd.DataFrame(transformed_data_list)
 
+    now = datetime.now()
+    dt_string = now.strftime("%d%m%Y%H%M%S")
+    dt_string = 'current_weather_data_portland_' + dt_string
+    df_data.to_csv(f"s3://weatherapiairflowyoutubebucket-yml/{dt_string}.csv", index=False, storage_options=aws_credentials)
+
+    
+    
 
 default_args = {
     'owner': 'airflow',
@@ -60,21 +92,16 @@ with DAG('weather_dag',
             http_conn_id = 'weathermap_api',
             endpoint="/data/2.5/weather?q=portland&appid=fc78a469d8c847ac9b7996c55b895e2b&units=metric",
             method = 'GET',
+
             response_filter= lambda r: json.loads(r.text),
             log_response=True
         )
 
         transform_weather_data = PythonOperator(
             task_id= 'transform_weather_data',
-            python_callable=logging_data
+            python_callable=transform_data
         )
         
-        
-        logging_weather_data = PythonOperator(
-            task_id= 'logging_weather_data',
-            python_callable=logging_data
-        )
-
 
 
 
